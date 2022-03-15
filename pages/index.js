372d6f1b942df6d19f6215d4e7d1bed4ff472f44
tauqirpasha <64155234+tauqirpasha@users.noreply.github.com>
2022-03-15 23:48:25 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
+
 
 export default function Home() {
   return (
@@ -17,7 +17,7 @@ export default function Home() {
         </p>
       </main>
 
-      <Footer />
+      
     </div>
   )
 }
